9ff736c2cb06fa77c1c855c064db70486e7ac73f
Đức Hoàng <duchoang@duchoang.local>
2023-10-18 01:29:06 +0700

    feat: integrate api upload

---
+++ internal/api/api.go
@@ -0,0 +1,82 @@
+package api
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+	"io"
+	"mime/multipart"
+	"net/http"
+	"os"
+	"path/filepath"
+
+	"github.com/duchoang206h/send-cli/config"
+)
+type Config struct {
+	
+}
+type ResultHttp struct {
+	Result string `json:"result"`
+}
+func UploadFile (ctx context.Context,  filePath string) (*ResultHttp, error){
+	uploadUrl:= fmt.Sprintf("%s/api/file", config.Config("API_URL"))
+	fmt.Println("uploadUrl", uploadUrl)
+	file, err := os.Open(filePath)
+
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+	var requestBody bytes.Buffer
+	writer := multipart.NewWriter(&requestBody)
+	part, err := writer.CreateFormFile("file", filepath.Base(filePath))
+
+	if err != nil {
+		return nil, err
+
+	}
+	_, err = io.Copy(part, file)
+
+	if err != nil {
+		return nil, err
+
+	}
+	err = writer.Close()
+
+	if err != nil {
+		return nil, err
+	}
+	request, err := http.NewRequestWithContext(ctx, "POST", uploadUrl , &requestBody)
+
+	if err != nil {
+		return nil, err
+
+	}
+	request.Header.Set("Content-Type", writer.FormDataContentType())
+	client := &http.Client{}
+	response, err := client.Do(request)
+	fmt.Println("response", response)
+	if err != nil {
+		return nil, err
+
+	}
+	defer response.Body.Close()
+
+	if response.StatusCode != http.StatusOK {
+		return nil, err
+	}
+	body, err := io.ReadAll(response.Body)
+
+	if err != nil {
+    return nil, err
+	}
+	var result ResultHttp
+	err = json.Unmarshal(body, &result)
+	fmt.Println("err", err)
+
+	if err != nil {
+		return nil, err
+	}
+	return &result, nil
+}
